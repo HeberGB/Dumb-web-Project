55c186a6e3d1ce1bf897c32dec1e7b45ea86d028
hebergb <hebergb98@gmail.com>
2017-11-05 06:19:33 -0600

    Ya esta terminado

--- index.js
+++ index.js
@@ -1,40 +1,142 @@
 var listaDeHexagramas = new Array(3); // Ya es un arreglo de tres elementos
-for (var i=0;i<listaDeHexagramas.length;i++) { // Todos van a tener 6
-	listaDeHexagramas[i] = new Array(6);
+var significados = [
+  'Cielo. Lo creativo. El principio generador',
+  'Tierra. Lo receptivo. El principio pasivo',
+  'Acumular. El obstáculo inicial. La dificultad del comienzo',
+  'Juventud. El joven necio. La inmadurez.',
+  'Esperarla espera. La maduración.',
+  'Disputar. El conflicto. El pleito.',
+  'Ejército. La legión.',
+  'Solidaridad. La unión',
+  'Animalito doméstico. La pequeña fuerza',
+  'Caminar. El porte. El paso cauteloso',
+  'Prosperidad. La paz. La armonía.',
+  'Cierre. El estancamiento. Lo inerte.',
+  'Hombres Reunidos. La unión comunitaria',
+  'Gran dominio. La gran posesión. Lo que se tiene de más.',
+  'Condescendencia. La modestia. La humildad',
+  'Ocuparse. El entusiasmo. La algarabía.',
+  'Conformarse. La continuidad. El seguimiento.',
+  'Destrucción. La reconstrucción. La labor en lo corrompido.',
+  'Acercarse. Lo que va llegando.',
+  'Observar. La contemplación.',
+  'Quebrar mordiendo. La dentellada. La filosa mordedura',
+  'Adornar. La elegancia. La gracia.',
+  'Resquebrajar. La desintegración. El derrumbe',
+  'Regresar. El retorno. Lo que vuelve.',
+  'Sinceridad. La inocencia. La naturalidad.',
+  'Fuerza educadora. El poder de lo fuerte. La gran acumulación.',
+  'Nutrirse. La alimentación. Las fauces.',
+  'Excesos. La preponderancia de lo grande.',
+  'Peligro. Lo abismal. La caída.',
+  'Distinguir. El resplandor. Lo adherente.',
+  'Unirla influencia. La atracción.',
+  'Luna Creciente. La duración. La permanencia.',
+  'Retirarse. EL repliegue.',
+  'Gran fuerza. El gran vigor.',
+  'Progresar. El avance.',
+  'Luz que se apaga. El oscurecimiento.',
+  'Gente de familia. El clan.',
+  'Contraste. La oposición. El antagonismo.',
+  'Dificultad. El obstáculo. El impedimento.',
+  'Explicar. La liberación. El alivio.',
+  'Perder. La disminución.',
+  'Evolución. El aumento. La ganancia.',
+  'Decidir. El desbordamiento. La resolución.',
+  'Encontrarse. El acoplamiento.',
+  'Cosechar. La reunión. La convergencia.',
+  'Subir. El ascenso. La escalada.',
+  'Angustia. La pesadumbre. El agotamiento.',
+  'El pozo de agua. La fuente.',
+  'Renovar. La revolución. El cambio',
+  'La caldera. Lo alquímico',
+  'Trueno. La conmoción. Lo suscitativo.',
+  'Cimientos. La quietud. La detención.',
+  'Evolución. El progreso gradual.',
+  'Desposar a la hija menor. La doncella.',
+  'Abundancia. La plenitud.',
+  'Viajero. El andariego',
+  'Viento. Lo penetrante. Lo suave.',
+  'Recogerse. La serenidad. La satisfacción.',
+  'Confusión. La dispersión. La disolución',
+  'Moderación. La restricción. La limitación',
+  'Fe Interior. La verdad interior. La sinceridad interna.',
+  'Pequeñas cosas importantes. La pequeña preponderancia.',
+  'Conclusiones. Después de la realización.',
+  'Inconcluso. Antes de la realización.'  
+];
+
+var nombres = [
+  "Ch'ien", "K´un", "Chug", "Meng", "Hsu", "Sung", "Shih", "Pi",
+  "Hsiao ch´u", "Lü", "Tai", "P´i", "T´ung jen", "Ta yu", "Ch´ien", "Yü", "Sui",
+  "Ku", "Lin", "Kuan", "Shih ho", "Pi gracias", "Po", "Pu", "Wu wang", "Ta chu",
+  "I", "TA KUO", "Kan", "Li", "Hsien", "Heng", "Tun", "Ta chuang", "Chin", "Ming",
+  "Chia jen", "K´uei", "Chien", "Hsieh", "Sun", "I chi", "Kuai", "Kou", "Ts´ui",
+  "Sheng", "k´un", "Ching", "Ko", "Ting", "Chen", "Ken", "Chien", "Kuei mei",
+  "Feng", "Lu", "Sun", "Tui", "Huan", "Chieh", "Chung fu", "Hsiao kuo", "Chi chi",
+  "We i chi"
+];
+
+for (var i = 0; i < listaDeHexagramas.length; i++) { // Todos van a tener 6
+  listaDeHexagramas[i] = new Array(6);
 }
 var cont = 0;
 var tieneMutacion = false; // Para saber si ya muto y así saber si imprimimos 1 o 3 hexagramas
 
 function agregarLinea() {
+  if (cont == 6) {
+    alert("Ya esta completo el hexagrama");
+    limpiarLineas();
+    return;
+  }
   var n1 = parseInt(document.formulario.n1.value);
   var n2 = parseInt(document.formulario.n2.value);
   var n3 = parseInt(document.formulario.n3.value);
-  if(sonCorrectasEntradas(n1,n2,n3)) {
-    var tipo = obtenerLinea(n1,n2,n3);
+  if (sonCorrectasEntradas(n1, n2, n3)) {
+    var tipo = obtenerLinea(n1, n2, n3);
 
-    for(var i=0;i<listaDeHexagramas.length;i++) {
+    for (var i = 0; i < listaDeHexagramas.length; i++) {
       listaDeHexagramas[i][cont] = tipo;
     }
 
-    if (tipo==1 || tipo==4) {
+    if (tipo == 1 || tipo == 4) {
       listaDeHexagramas[1][cont] = obtenerMutacion1(tipo); // Como ya no tengo que crear nuevos hexagramas entonces se reduce aqui
       listaDeHexagramas[2][cont] = obtenerMutacion2(tipo);
       tieneMutacion = true;
     }
-      cont++;
-    if(tieneMutacion == false) {  // Para imprimir solo un hexagrama
-    	var padre = document.getElementById("Hexagrama");
-    	padre.innerHTML = "";
-    	var hijo = document.createElement("div");
-    	padre.appendChild(hijo);
-    	imprimirHexagrama(listaDeHexagramas[0],hijo);
+    cont++;
+    if (tieneMutacion == false) {  // Para imprimir solo un hexagrama
+      var padre = document.getElementById("container-hexa");
+      padre.innerHTML = "";
+      var hijo = document.createElement("div");
+      hijo.className = "hexagrama";
+      hijo.id = 2;
+      padre.appendChild(hijo);
+      imprimirHexagrama(listaDeHexagramas[0], hijo);
     }
     else { // Para imprimir los tres hexagramas
-    	imprimirHexagramas();
+      imprimirHexagramas();
     }
-    // Aqui supongo que le agregaremos un if cont==5, para saber que se completó el hexagrama,
-    // mandamos los hexagramas y obtenemos el numero de cada uno, y ya de ahí el nombre
-    // si necesitas ayuda metiendo los nombres y las descripciones me dices
+    console.log(cont);
+    if (cont == 6) {
+      var valor2 = valorHexagrama(document.getElementById("hexagrama-2"));
+      var hexagrama = document.getElementById("hexagrama-2");
+      hexagrama.addEventListener('mouseover', function () {
+        mostrarDescripcion(valor2);
+      });
+      prenderCelda(valor2);
+      hexagrama.appendChild(document.createTextNode(String(valor2) + ". " + nombres[valor2 - 1]));
+      
+      if (document.getElementById("hexagrama-1")) {
+        var valor1 = valorHexagrama(document.getElementById("hexagrama-1"));
+        var hexagrama = document.getElementById("hexagrama-1");
+        hexagrama.addEventListener('mouseover', function () {
+          mostrarDescripcion(valor1);
+        });
+        prenderCelda(valor1);
+        hexagrama.appendChild(document.createTextNode(String(valor1) + ". " + nombres[valor1 - 1]));
+      }
+    }
   }
   else {
     alert("No introdujiste valores correctos");
@@ -42,17 +144,17 @@ function agregarLinea() {
   limpiarLineas();
 }
 
-function sonCorrectasEntradas(n1,n2,n3) {
-  if(n1!=2 && n1!=3) {
+function sonCorrectasEntradas(n1, n2, n3) {
+  if (n1 != 2 && n1 != 3) {
     return false;
   }
-  if(n2!=2 && n2!=3) {
+  if (n2 != 2 && n2 != 3) {
     return false;
   }
-  if(n3!=2 && n3!=3) {
+  if (n3 != 2 && n3 != 3) {
     return false;
   }
-    return true;
+  return true;
 }
 
 function limpiarLineas() {
@@ -62,58 +164,58 @@ function limpiarLineas() {
 function imprimirHexagramas() {
   var padre = document.getElementById("container-hexa");
   padre.innerHTML = "";
-  for (var i=0;i<listaDeHexagramas.length;i++) {
+  for (var i = 0; i < listaDeHexagramas.length; i++) {
     var hijo = document.createElement("div");
     hijo.className = "hexagrama";
-    hijo.id = i;
+    hijo.id = "hexagrama-" + String(i);
     padre.appendChild(hijo);
-    imprimirHexagrama(listaDeHexagramas[i],hijo);
+    imprimirHexagrama(listaDeHexagramas[i], hijo);
   }
 }
 
 function obtenerMutacion1(tipo) {
-  if(tipo==1) {
+  if (tipo == 1) {
     return 3;
   }
-  else{
+  else {
     return 2;
   }
 }
 
 function obtenerMutacion2(tipo) {
-  if(tipo==1) {
+  if (tipo == 1) {
     return 2;
   }
-  else{
+  else {
     return 3;
   }
 }
 
-function obtenerLinea(n1,n2,n3){
-  var suma = n1+n2+n3;
-  if(suma==6) {
+function obtenerLinea(n1, n2, n3) {
+  var suma = n1 + n2 + n3;
+  if (suma == 6) {
     return 1;
   }
-  else if(suma==7) {
+  else if (suma == 7) {
     return 2;
   }
-  else if(suma==8) {
+  else if (suma == 8) {
     return 3;
   }
-  else if(suma==9){
+  else if (suma == 9) {
     return 4;
   }
 }
 
-function imprimirHexagrama(hexagrama,padre) {
+function imprimirHexagrama(hexagrama, padre) {
   var parrafo;
   var texto;
   padre.innerHTML = "";
-  for(var i=hexagrama.length-1; i>=0; i--) {
+  for (var i = hexagrama.length - 1; i >= 0; i--) {
     imagen = document.createElement("IMG");
-    if(obtenerImagen(hexagrama[i])){
+    if (obtenerImagen(hexagrama[i])) {
       imagen.src = obtenerImagen(hexagrama[i]);
-      imagen.id = "image";
+      imagen.className = "image";
     }
     padre.appendChild(imagen);
     saltoLinea = document.createElement("br");
@@ -122,16 +224,16 @@ function imprimirHexagrama(hexagrama,padre) {
 }
 
 function obtenerImagen(numero) {
-  if(numero==1) {
+  if (numero == 1) {
     return "imagen1.png";
   }
-  else if(numero==2) {
+  else if (numero == 2) {
     return "imagen2.png";
   }
-  else if(numero==3) {
+  else if (numero == 3) {
     return "imagen3.png";
   }
-  else if(numero==4) {
+  else if (numero == 4) {
     return "imagen4.png";
   }
   else {
@@ -142,22 +244,208 @@ function obtenerImagen(numero) {
 function borrarHexagrama() {
   cont = 0;
   listaDeHexagramas = new Array(3);
-  for (var i=0;i<listaDeHexagramas.length;i++) { // Todos van a tener 6
+  tieneMutacion = false;
+  for (var i = 0; i < listaDeHexagramas.length; i++) { // Todos van a tener 6
     listaDeHexagramas[i] = new Array(6);
   }
   var padre = document.getElementById("container-hexa");
   padre.innerHTML = "";
 }
 
-function valor() {
+function union(hexSuperior, hexInferior) {
+  while (hexInferior.hasChildNodes()) {
+    hexSuperior.id = "";
+    hexSuperior.appendChild(hexInferior.firstChild);
+  }
 
 }
 
-function union(hexSuperior, hexInferior) {
-  while(hexInferior.hasChildNodes()){
-    hexSuperior.id="";
-    hexSuperior.appendChild(hexInferior.firstChild);
-    console.log(hexSuperior);
+function valorHexagrama(hexa) {
+  var images = hexa.getElementsByClassName("image");
+  var unaLinea = "file:///C:/Users/hjgb/dev/Proyecto%20Web%20(Heber%20es%20gay)/imagen2.png";
+  var dosLineas = "file:///C:/Users/hjgb/dev/Proyecto%20Web%20(Heber%20es%20gay)/imagen3.png";
+  console.log(valorPorSeccion("superior"));
+  console.log(valorPorSeccion("inferior"));
+  return cambiandoValor(String(valorPorSeccion("superior")) + String(valorPorSeccion("inferior")));
+  function valorPorSeccion(seccion) {
+    var i = seccion == "superior" ? 0 : 3;
+    if (images[i + 0].src == unaLinea) {
+      if (images[i + 1].src == unaLinea) {
+        if (images[i + 2].src == unaLinea) {
+          return 1;
+        } else if (images[i + 2].src == dosLineas) {
+          return 6;
+        }
+      } else if (images[i + 1].src == dosLineas) {
+        if (images[i + 2].src == dosLineas) {
+          return 4;
+        } else if (images[i + 2].src == unaLinea) {
+          return 7;
+        }
+      }
+    } else if (images[i + 0].src == dosLineas) {
+      if (images[i + 1].src == dosLineas) {
+        if (images[i + 2].src == dosLineas) {
+          return 5;
+        } else if (images[i + 2].src == unaLinea) {
+          return 2;
+        }
+      } else if (images[i + 1].src == unaLinea) {
+        if (images[i + 2].src == dosLineas) {
+          return 3;
+        } else if (images[i + 2].src == unaLinea) {
+          return 8;
+        }
+      }
+    }
   }
-  
+  function cambiandoValor(valor) {
+    switch (valor) {
+      case "11":
+        return 1;
+      case "12":
+        return 25;
+      case "13":
+        return 6;
+      case "14":
+        return 33;
+      case "15":
+        return 12;
+      case "16":
+        return 44;
+      case "17":
+        return 13;
+      case "18":
+        return 10;
+      case "21":
+        return 34;
+      case "22":
+        return 51;
+      case "23":
+        return 40;
+      case "24":
+        return 62;
+      case "25":
+        return 16;
+      case "26":
+        return 32;
+      case "27":
+        return 55;
+      case "28":
+        return 54;
+      case "31":
+        return 5;
+      case "32":
+        return 3;
+      case "33":
+        return 29;
+      case "34":
+        return 39;
+      case "35":
+        return 8;
+      case "36":
+        return 48;
+      case "37":
+        return 63;
+      case "38":
+        return 60;
+      case "41":
+        return 26;
+      case "42":
+        return 27;
+      case "43":
+        return 4;
+      case "44":
+        return 52;
+      case "45":
+        return 23;
+      case "46":
+        return 18;
+      case "47":
+        return 22;
+      case "48":
+        return 41;
+      case "51":
+        return 11;
+      case "52":
+        return 24;
+      case "53":
+        return 7;
+      case "54":
+        return 15;
+      case "55":
+        return 2;
+      case "56":
+        return 46;
+      case "57":
+        return 36;
+      case "58":
+        return 19;
+      case "61":
+        return 9;
+      case "62":
+        return 42;
+      case "63":
+        return 59;
+      case "64":
+        return 53;
+      case "65":
+        return 20;
+      case "66":
+        return 57;
+      case "67":
+        return 37;
+      case "68":
+        return 61;
+      case "71":
+        return 14;
+      case "72":
+        return 21;
+      case "73":
+        return 64;
+      case "74":
+        return 56;
+      case "75":
+        return 35;
+      case "76":
+        return 50;
+      case "77":
+        return 30;
+      case "78":
+        return 38;
+      case "81":
+        return 43;
+      case "82":
+        return 17;
+      case "83":
+        return 47;
+      case "84":
+        return 31;
+      case "85":
+        return 45;
+      case "86":
+        return 28;
+      case "87":
+        return 49;
+      case "88":
+        return 58;
+    }
+  }
+}
+
+function prenderCelda(valor) {
+  console.log("si se ejecuto");
+  console.log(valor);
+  var tabla = document.getElementById("table-hexa");
+  var valores = tabla.getElementsByTagName("td");
+  for (let i = 0 ; i < valores.length ; i++){
+    console.log(String(valor) == valores[i].textContent)
+    if (String(valor) == valores[i].textContent) {
+      valores[i].className = "seleccionado";
+    }
+  }
+}
+
+function mostrarDescripcion(valor) {
+  alert(significados[valor-1]);
 }
